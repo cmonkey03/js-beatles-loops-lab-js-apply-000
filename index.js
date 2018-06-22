93a9c10b2a0472f4055cd31962f45389990d25a4
russellpbrown@gmail.com <russellpbrown@gmail.com>
2018-06-22 03:41:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < 4; i++) {
+    array.push(`${musicians[0]} plays ${instruments[0]}`)
+    musicians.shift()
+    instruments.shift()
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+ var excitedFacts = []
+ var factsIndex = 0
+ while (excitedFacts.length < facts.length) {
+   excitedFacts.push(`${facts[factsIndex]}!!!`)
+   factsIndex += 1
+ } 
+ return excitedFacts
+}
+
+function iLoveTheBeatles(n) {
+  var array = []
+  do {
+    array.push("I love the Beatles!") 
+  } while (n < 15) {
+    n +=1
+  }
+  return array
+}
